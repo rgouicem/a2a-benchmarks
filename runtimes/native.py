98e21932e158a8319477f74cf974f704b1116cef
Redha Gouicem <redha.gouicem@gmail.com>
2021-11-02 15:30:54 +0100

    runtime: native: check if specified architecture matches the host

--- runtimes/native.py
+++ runtimes/native.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 from runtimes.runtime import Runtime
+import platform, logging
 
 class Native(Runtime):
 
     def __init__(self, args, config):
         super().__init__(args, config)
+
+        if platform.machine() != args.arch:
+            logging.error(f"Cannot execute {args.arch} binaries on a {platform.machine()} architecture with the 'native' runtime.")
+            exit(1)
 
         self.cmdline = []
 
