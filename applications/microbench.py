cfb6ea29ba36b44057f1a1c87e092f4ddddc875f
Redha Gouicem <redha.gouicem@gmail.com>
2021-11-23 11:03:02 +0100

    apps: micro: fix support for sqlite-bench

--- applications/microbench.py
+++ applications/microbench.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import logging
+import logging, os
 import pandas as pd
 
 from applications.bench import Benchmark
@@ -68,10 +68,74 @@ class Math(Benchmark):
         return ret
 
 
+class Sqlite(Benchmark):
+
+    app = None
+    arch = None
+    binary_path = None
+
+    def __init__(self, args, config):
+        super().__init__(args, config)
+        self.app = args.bench[6:]
+        self.binary_path = config.store['SQLITE_BENCH_BIN']
+        self.binary_dir = os.path.dirname(self.binary_path)
+
+        # Get binary ISA
+        if args.arch not in archs:
+            logging.error("Architecture not supported by micro.math. Should be among "+str(archs))
+            exit(1)
+        self.arch = args.arch
+
+
+    def prepare(self):
+        super().prepare()
+
+        self.cmdline = [ self.binary_path ] + [ f"{self.binary_dir}/test{i}.sql" for i in range(1, 17) ]
+
+
+    def format_output(self, stdout, stderr):
+        df = pd.DataFrame()
+        stdout.seek(0)
+        for l in stdout:
+            if "bench.py" in l:
+                retval = int(l.split(' ')[7])
+            else:
+                run, res = l.strip().split(';')
+                test_id, hue = run.split('-')
+                df = df.append({ 'bench': f"micro.{self.app}-{run}",
+                                 'dataset': 'none',
+                                 'arch': self.arch,
+                                 'threads': 1,
+                                 'cmdline': ' '.join(self.cmdline),
+                                 'unit': 'ms',
+                                 'value': float(res) }, ignore_index=True)
+
+        df['retval'] = retval
+        if len(df) == 0:
+            return None
+        return df
+
+
+    def cleanup(self):
+        pass
+
+
+    def __str__(self):
+        ret = "<"
+        ret += "name="+self.name
+        ret += ", threads="+str(self.threads)
+        ret += ", dataset=none"
+        ret += ", arch="+self.arch
+        ret += ", cmdline: " + str(self.cmdline)
+        ret += ">"
+        return ret
+
+
 class MicrobenchFactory():
 
     apps = {
         'micro.math': Math,
+        'micro.sqlite': Sqlite,
     }
 
     def create(args, config):
